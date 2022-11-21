74c603746ab8210e2c15d700734c50a4059e900b
Vadym <vadim.strutovskii@gmail.com>
2022-11-21 21:49:27 +0200

    1

--- resources/views/name/master.blade.php
+++ resources/views/name/master.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ Config::get('app.locale') }}">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>@yield('InfiniteName')</title>
     <link rel="shortcut icon" href="{{asset('/')}}name/images/logo.png">
     <link rel="stylesheet"href="{{asset('/')}}name/css/bootstrap.min.css">
